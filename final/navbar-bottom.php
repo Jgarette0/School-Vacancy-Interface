f1575b71106867b4d8b64366255678b0b4c8bf26
Jgarette0 <jiliangarette@gmail.com>
2023-10-09 03:20:33 +0800

    added navbar bottom icons

--- final/navbar-bottom.php
+++ final/navbar-bottom.php
@@ -1,14 +1,35 @@
 <nav class="navbarBottom">
   <a href="#home" class="navItem active">
-    <img src="home.svg" alt="Home">
+  <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
+  <lord-icon
+    src="https://cdn.lordicon.com/slduhdil.json"
+    trigger="click"
+    colors="primary:#171717"
+    state="hover-3"
+    style="width:40px;height:40px; margin-bottom: 4px">
+</lord-icon>
     <span>Home</span>
   </a>
-  <a href="#explore" class="navItem">
-    <img src="explore.svg" alt="Explore">
-    <span>Explore</span>
+  <a href="#list" class="navItem">
+  <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
+<lord-icon
+    src="https://cdn.lordicon.com/zncllhmn.json"
+    trigger="click"
+    colors="primary:#171717"
+    state="hover"
+    style="width:40px;height:40px; margin-bottom: 4px">
+</lord-icon>
+    <span>List</span>
   </a>
   <a href="#setting" class="navItem">
-    <img src="setting.svg" alt="setting">
-    <span>Setting</span>
+  <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
+<lord-icon
+    src="https://cdn.lordicon.com/dycatgju.json"
+    trigger="click"
+    colors="primary:#171717"
+    state="hover-2"
+    style="width:40px;height:40px; margin-bottom: 4px">
+</lord-icon>
+  <span>Setting</span>
   </a>
 </nav>
