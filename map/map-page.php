22fb7a4474d0ad2a1040fefd61d5336a797f3a68
Jgarette0 <jiliangarette@gmail.com>
2023-10-18 18:13:46 +0800

    adding a button that shows different layout in different floors

--- map/map-page.php
+++ map/map-page.php
@@ -9,8 +9,92 @@
   <link rel="stylesheet" href="../css/navbar-bottom.css">
 </head>
   <body>
-    <?php include '../partials/navbar.php';?>
-    <?php include '../partials/navbar-bottom.php';?>
+  <nav>
+      <div class="navbar" id="navbar">
+      <a href="../index.php">
+          <div class="profile-holder">
+            <button id="profileBtn">
+            <script src="https://cdn.lordicon.com/lordicon-1.1.0.js"></script>
+            <lord-icon
+                src="https://cdn.lordicon.com/zxvuvcnc.json"
+                trigger="click"
+                style="width:40px;height:40px; margin-bottom:-3px;">
+            </lord-icon>
+            </button>
+          </div>
+        </a>
+        <div class="search-container">
+          <form class="searchform" action="../search/search.php" method="post">
+            <a>
+              <button type="submit" class="submits">
+                <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
+                <lord-icon src="https://cdn.lordicon.com/msoeawqm.json" trigger="click" colors="primary:#121331,secondary:#242424" style="width:30px;height:30px;">
+                </lord-icon>
+              </button>
+              </a>
+              <a>
+                <input id="search" type="text" name="usersearch" placeholder="Search...">
+            </a>
+          </form>
+        </div>
+      </div>
+    </nav>
+    <nav class="navbarBottom">
+<div class="radio-container">
+<div class="radio-inputs">
+		<label>
+			<input class="radio-input" type="radio" name="engine">
+				<span class="radio-tile">
+        <a href="">
+					<script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
+            <lord-icon
+              src="https://cdn.lordicon.com/dycatgju.json"
+              trigger="click"
+              colors="primary:#171717"
+              state="hover-2"
+              style="width:32px;height:32px;  margin-bottom: -5px">
+            </lord-icon>
+					<span class="radio-label">Settings</span>
+          </a>
+				</span>
+		</label>
+    <label>
+			<input class="radio-input" type="radio" name="engine">
+			<span class="radio-tile">
+      <a href="../index.php">
+        <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
+        <lord-icon
+      src="https://cdn.lordicon.com/slduhdil.json"
+      trigger="click"
+      colors="primary:#171717"
+      state="hover-3"
+      style="width:32px;height:32px; margin-bottom: -5px">
+    </lord-icon>
+				<span class="radio-label">Home</span>
+        </a>
+			</span>
+		</label>
+    
+		<label>
+			<input checked="" class="radio-input" type="radio" name="engine">
+			<span class="radio-tile">
+      <a href="../show-vacants/vacants.php">
+				<script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
+        <lord-icon
+          src="https://cdn.lordicon.com/zncllhmn.json"
+          trigger="click"
+          colors="primary:#171717"
+          state="hover"
+          style="width:32px;height:32px; margin-bottom: -5px">
+        </lord-icon>
+    <span class="radio-label">Vacants</span>	
+    </a>
+			</span>
+		</label>
+
+</div>
+  </div>
+</nav>
     <div class="top-view">
     <div class="layout-container"> 
       <div class="layout">
